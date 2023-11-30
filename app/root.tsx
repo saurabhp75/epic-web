f3a646c1ee57da786301c7183e48992d26aa058e
Saurabh Prakash <saurabhp75@gmail.com>
2023-11-30 17:40:37 +0530

    Add tailwind to project

--- app/root.tsx
+++ app/root.tsx
@@ -3,6 +3,7 @@ import { Links, LiveReload, Scripts } from '@remix-run/react'
 
 import faviconAssetUrl from './assets/favicon.svg'
 import fontStylesheetUrl from './styles/font.css'
+import tailwindStylesheetUrl from './styles/tailwind.css'
 
 // By default Remix takes the "favicon.ico" from "/public'
 // folder, we can change this usings the links export
@@ -15,6 +16,7 @@ export const links: LinksFunction = () => {
 			rel: 'icon',
 		},
 		{ rel: 'stylesheet', href: fontStylesheetUrl },
+		{ rel: 'stylesheet', href: tailwindStylesheetUrl },
 	]
 }
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,17 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+import { type Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme.js'
 
+export default {
+	content: ['./app/**/*.{ts,tsx,jsx,js}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: [
+					'Nunito Sans',
+					'Nunito Sans Fallback',
+					...defaultTheme.fontFamily.sans,
+				],
+			},
+		},
+	},
+} satisfies Config
