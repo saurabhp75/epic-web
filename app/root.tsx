4e373c62928a6d46ec323c39aa010d3fca0c60a2
Saurabh Prakash <saurabhp75@gmail.com>
2023-11-30 16:25:14 +0530

    Add links for custom favicon

--- app/root.tsx
+++ app/root.tsx
@@ -1,9 +1,25 @@
-import { LiveReload, Scripts } from '@remix-run/react'
+import { type LinksFunction } from '@remix-run/node'
+import { Links, LiveReload, Scripts } from '@remix-run/react'
+
+// By default Remix takes the "favicon.ico" from "/public'
+// folder, we can change this usings the links export
+
+export const links: LinksFunction = () => {
+	return [
+		{
+			type: 'image/svg+xml',
+			href: 'favicon.svg',
+			rel: 'icon',
+		},
+	]
+}
 
 export default function App() {
 	return (
 		<html lang="en">
-			<head></head>
+			<head>
+				<Links />
+			</head>
 			<body>
 				<p>Hello World!!!</p>
 				<Scripts />
