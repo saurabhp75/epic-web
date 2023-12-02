52e46200bd41cb05c2a16f718093f605883f1a73
Saurabh Prakash <saurabhp75@gmail.com>
2023-12-02 22:47:18 +0530

    Add error messages for file related fields

--- app/routes/users+/$username_+/notes.$noteId_.edit.tsx
+++ app/routes/users+/$username_+/notes.$noteId_.edit.tsx
@@ -270,6 +270,9 @@ function ImageChooser({
 							/>
 						</label>
 					</div>
+					<div className="min-h-[32px] px-4 pb-3 pt-1">
+						<ErrorList id={fields.file.errorId} errors={fields.file.errors} />
+					</div>
 				</div>
 				<div className="flex-1">
 					<Label htmlFor={fields.altText.id}>Alt Text</Label>
@@ -278,6 +281,9 @@ function ImageChooser({
 						onChange={e => setAltText(e.currentTarget.value)}
 					/>
 				</div>
+			</div>
+			<div className="min-h-[32px] px-4 pb-3 pt-1">
+				<ErrorList id={config.errorId} errors={config.errors} />
 			</div>
 		</fieldset>
 	)
