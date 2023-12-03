eef27f2c20c3e28e6895aee8994d1d2c082655f5
Saurabh Prakash <saurabhp75@gmail.com>
2023-12-03 18:05:56 +0530

    Add nested query in seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -8,42 +8,36 @@ const prisma = new PrismaClient()
 // delete all the notes and images associated with the user as well.
 await prisma.user.deleteMany()
 
-// 🐨 create a new user with the following properties:
-// email: 'kody@kcd.dev',
-// username: 'kody',
-// name: 'Kody',
-
-const kody = await prisma.user.create({
+await prisma.user.create({
 	data: {
 		email: 'kody@kcd.dev',
 		username: 'kody',
 		name: 'Kody',
-	},
-})
-
-// 🐨 instead of finding the first note like we do above, let's  "create"
-await prisma.note.create({
-	data: {
-		id: 'd27a197e',
-		title: 'Basic Koala Facts',
-		content:
-			'Koalas are found in the eucalyptus forests of eastern Australia. They have grey fur with a cream-coloured chest, and strong, clawed feet, perfect for living in the branches of trees!',
-		ownerId: kody.id,
-		images: {
+		notes: {
 			create: [
 				{
-					altText: 'an adorable koala cartoon illustration',
-					contentType: 'image/png',
-					blob: await fs.promises.readFile(
-						'./tests/fixtures/images/kody-notes/cute-koala.png',
-					),
-				},
-				{
-					altText: 'a cartoon illustration of a koala in a tree eating',
-					contentType: 'image/png',
-					blob: await fs.promises.readFile(
-						'./tests/fixtures/images/kody-notes/koala-eating.png',
-					),
+					id: 'd27a197e',
+					title: 'Basic Koala Facts',
+					content:
+						'Koalas are found in the eucalyptus forests of eastern Australia. They have grey fur with a cream-coloured chest, and strong, clawed feet, perfect for living in the branches of trees!',
+					images: {
+						create: [
+							{
+								altText: 'an adorable koala cartoon illustration',
+								contentType: 'image/png',
+								blob: await fs.promises.readFile(
+									'./tests/fixtures/images/kody-notes/cute-koala.png',
+								),
+							},
+							{
+								altText: 'a cartoon illustration of a koala in a tree eating',
+								contentType: 'image/png',
+								blob: await fs.promises.readFile(
+									'./tests/fixtures/images/kody-notes/koala-eating.png',
+								),
+							},
+						],
+					},
 				},
 			],
 		},
