443a7ae40dc99eb14722e2674c1568722f8b6388
Saurabh Prakash <saurabhp75@gmail.com>
2023-12-04 22:18:26 +0530

    Add optimistic theme switch

--- app/root.tsx
+++ app/root.tsx
@@ -15,6 +15,7 @@ import {
 	Scripts,
 	ScrollRestoration,
 	useFetcher,
+	useFetchers,
 	useLoaderData,
 	useMatches,
 } from '@remix-run/react'
@@ -133,7 +134,7 @@ export async function action({ request }: DataFunctionArgs) {
 function App() {
 	// throw new Error('🐨 Loader error')
 	const data = useLoaderData<typeof loader>()
-	const theme = data.theme
+	const theme = useTheme()
 	const matches = useMatches()
 	const isOnSearchPage = matches.find(m => m.id === 'routes/users+/index')
 
@@ -175,6 +176,27 @@ function App() {
 			<Spacer size="3xs" />
 		</Document>
 	)
+}
+
+// useTheme hook reads the current theme from useLoaderData
+// and returns it unless there's an ongoing fetcher setting the theme.
+// 🦉 The ThemeSwitch is using useFetcher to make the switch.
+// 💰 Add a `.find` on the fetchers array to find the fetcher which has formData
+// with an intent of 'update-theme'. If that fetcher is found, then return the
+// 'theme' from the fetcher's formData.
+function useTheme() {
+	const data = useLoaderData<typeof loader>()
+	const fetchers = useFetchers()
+	const themeFetcher = fetchers.find(
+		fetcher => fetcher.formData?.get('intent') === 'update-theme',
+	)
+	// Get the value submitted by the user
+	const optimisticTheme = themeFetcher?.formData?.get('theme')
+	if (optimisticTheme === 'light' || optimisticTheme === 'dark') {
+		return optimisticTheme
+	}
+	// Fallback to server provided value
+	return data.theme
 }
 
 export default function AppWithProviders() {
