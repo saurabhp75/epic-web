08b6b8ea8c73f35a2088db33f59a676d63c8068c
Saurabh Prakash <saurabhp75@gmail.com>
2023-12-20 22:02:13 +0530

    Handle connection exceptions

--- app/routes/_auth+/auth.$provider.callback.ts
+++ app/routes/_auth+/auth.$provider.callback.ts
@@ -1,7 +1,8 @@
 import { type LoaderFunctionArgs } from '@remix-run/node'
-import { authenticator } from '#app/utils/auth.server'
+import { authenticator, getUserId } from '#app/utils/auth.server'
 import { ProviderNameSchema, providerLabels } from '#app/utils/connections'
 import { redirectWithToast } from '#app/utils/toast.server'
+import { prisma } from '#app/utils/db.server'
 
 export async function loader({ request, params }: LoaderFunctionArgs) {
 	const providerName = ProviderNameSchema.parse(params.provider)
@@ -24,7 +25,32 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
 	// to the login page with a toast message indicating that there was an error
 	// authenticating with the provider.
 
-	console.log({ profile })
+	// console.log({ profile })
+
+	// check db for an existing connection
+	// via the providerName and providerId (profile.id) and select the userId
+	const existingConnection = await prisma.connection.findUnique({
+		select: { userId: true },
+		where: {
+			providerName_providerId: { providerName, providerId: profile.id },
+		},
+	})
+
+	const userId = await getUserId(request)
+
+	// if there's an existing connection and a userId, then there's a conflict... Either:
+	// 1. The account is already connected to their own account
+	// 2. The account is already connected to someone else's account
+	// redirect to /settings/profile/connections with apprpropriate toast message
+	if (existingConnection && userId) {
+		throw await redirectWithToast('/settings/profile/connections', {
+			title: 'Already Connected',
+			description:
+				existingConnection.userId === userId
+					? `Your "${profile.username}" ${label} account is already connected.`
+					: `The "${profile.username}" ${label} account is already connected to another account.`,
+		})
+	}
 
 	throw await redirectWithToast('/login', {
 		title: 'Auth Success (jk)',
