f1711377edaf87c9aa6281cfcc11561a136bf039
Saurabh Prakash <saurabhp75@gmail.com>
2023-12-27 01:00:00 +0530

    Add authenticated e2e test

---
+++ tests/e2e/2fa.test.ts
@@ -0,0 +1,58 @@
+import { faker } from '@faker-js/faker'
+import * as setCookieParser from 'set-cookie-parser'
+import { getSessionExpirationDate, sessionKey } from '#app/utils/auth.server'
+import { prisma } from '#app/utils/db.server'
+import { invariant } from '#app/utils/misc'
+import { expect, test } from '#tests/playwright-utils'
+import { sessionStorage } from '#app/utils/session.server'
+
+test('Users can add 2FA to their account and use it when logging in', async ({
+	page,
+	insertNewUser,
+}) => {
+	const password = faker.internet.password()
+	const user = await insertNewUser({ password })
+	invariant(user.name, 'User name is not defined')
+
+	// by passing the cookieConfig to addCookies (note, you need to add the domain!)
+	// so you can use {...cookieConfig, domain: 'localhost'}
+
+	// The cookieConfig type can't be specific enough to satisfy the
+	// requirements of addCookies. If you'd like to parse the cookieConfig with
+	// zod to make it more type safe, be my guest, but I don't think it's necessary
+	// in this case. Simply add `as any` to the cookieConfig and you'll be good to go.
+
+	// create a new session for the user using prisma.session.create
+	// you can reference the login utility from auth.server.ts if you need a reminder.
+	const session = await prisma.session.create({
+		data: {
+			expirationDate: getSessionExpirationDate(),
+			userId: user.id,
+		},
+		select: { id: true },
+	})
+
+	// get the cookieSession from sessionStorage.getSession
+	const cookieSession = await sessionStorage.getSession()
+	// set the sessionKey on the cookieSession to the session.id
+	cookieSession.set(sessionKey, session.id)
+	// parse the setCookie header using setCookieParser.parseString
+	const cookieConfig = setCookieParser.parseString(
+		// commit the cookieSession using sessionStorage.commitSession
+		await sessionStorage.commitSession(cookieSession),
+	) as any
+
+	// Uncomment to see the contents of the cookie
+	// throw new Error(JSON.stringify({ cookieConfig }, null, 2))
+
+	// add the cookie to the browser context using page.context().addCookies
+	await page.context().addCookies([{ ...cookieConfig, domain: 'localhost' }])
+
+	await page.goto('/settings/profile')
+
+	await page.getByRole('link', { name: /enable 2fa/i }).click()
+
+	await expect(page).toHaveURL(`/settings/profile/two-factor`)
+	const main = page.getByRole('main')
+	await main.getByRole('button', { name: /enable 2fa/i }).click()
+})
